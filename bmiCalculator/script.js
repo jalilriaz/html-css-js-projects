86a45f010c2452b5ae61c3b8891e2596fabf95f6
Jalil Riaz <jalilriaz990@gmail.com>
2025-03-07 14:32:21 +0500

    4 more files

---
+++ bmiCalculator/script.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form")
+form.addEventListener("submit", function (e) {
+  e.preventDefault()
+
+  const height = parseInt(document.querySelector("#height").value)
+  const weight = parseInt(document.querySelector("#weight").value)
+  const results = document.querySelector("#results")
+
+  if (height === "") {
+    results.innerHTML = `Plese give a valid height  ${height}`
+  } else if (weight === "") {
+    results.innerHTML = `Please give a valid weight ${weight}`
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2)
+    results.innerHTML = `<span>${bmi}</span>`
+  }
+})
